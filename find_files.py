2ad68890fb716ab0cb6ef12242bbbd9c736d674d
erikpruess <73554777+erikpruess@users.noreply.github.com>
2021-03-07 14:31:49 +0100

    Update find_files.py

--- find_files.py
+++ find_files.py
@@ -2,21 +2,21 @@ import os
 
 
 # execute the find ~/ function in terminal
-# select with command a, paste into text filename
+# select with command a, paste into text file
 # copy path
 
 def list_to_string(liste, seperator=''):
     return seperator.join(liste)
 
-
+# paste path            here
 terminal_list = open('/Users/erikpreuss/Desktop/clash/princess.txt', 'r')
 terminal_string = terminal_list.read().replace('\n', '')
 terminal_list.close()
 terminal_string = terminal_string.split('/')
 path = []
 for i in terminal_string:
-    # write here the file you want to find
-    if i == 'Latein-27.apkg':
+    # write here the file/folder you want to find
+    if i == 'User':
         place = terminal_string.index(i) - 1
         while terminal_string[place] != 'Users':
             path.append(terminal_string[place] + '/')
